19be1e762fd74297b622d54ab6b348df4aa57934
Meenakshi <vmeenakshi.aayacare.com>
2023-10-30 06:29:49 +0530

    Initial commit

---
+++ Client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Resturant from "./component/restaurant";
+
+
+
+const App = () => {
+return( 
+< Resturant />
+
+
+);
+  
+};
+
+export default App;
---
+++ Client/src/component/menuApi.js
@@ -0,0 +1,129 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/masala-dosa.jpg",
+      name: "Masala Dosa with Chutney",
+      category: "breakfast",
+      day: "MONDAY",
+      description:
+        "It Gives a combination of Masala Dosa, Chutney, Sambar, Bread Jam, Bread Omelete, Cut Fruits, Corn Flakes and Milk ",
+    },
+    {
+        id: 2,
+        image: "images/big-thali.webp",
+        name: "Roti, Gravy Sabzi and Flavoured Rice",
+        category: "lunch",
+        day: "MTWThFS",
+        description:
+          "It Gives a combination of Roti, White Rice, Flavoured Rice, Dry Sabzi(MWFS), Wet Sabzi, Dal, Sambar, Rasam, Buttermilk, Salad and Papad ",
+      },
+      {
+        id: 3,
+        image: "images/expect.gif",
+        name: "Coffee and Biscuit",
+        category: "Snacks",
+        day: "All Day",
+        description:
+          "It Gives a combination of Tea, coffee, Milk, and biscuit",
+      },
+      {
+        id: 4,
+        image: "images/mini-thali.jpg",
+        name: "Roti, Gravy Sabzi and Salad",
+        category: "dinner",
+        day: "MTWThS",
+        description:
+          "It Gives a combination of Roti, White Rice, Dry Sabzi(MW), Wet Sabzi, Dal, Rasam, Salad and Papad ",
+      },
+    {
+        id: 5,
+        image: "images/paratha.jpg",
+        name: "Methi Paratha with Tomato Chutney",
+        category: "breakfast",
+        day: "TUESDAY",
+        description:
+          "It Gives a combination of Methi Paratha,Tomato Chutney, Upma, Bread Jam, Bread Omelete, Cut Fruits, Corn Flakes and Milk ",
+      },
+      {
+        id: 6,
+        image: "images/idly.webp",
+        name: "Idly, Vada and Chutney",
+        category: "breakfast",
+        day: "WEDNESDAY",
+        description:
+          "It Gives a combination of Idly, Vada, Chutney, Sambar, Bread Jam, Bread Omelete, Cut Fruits, Corn Flakes and Milk ",
+      },
+      {
+        id: 7,
+        image: "images/aloo-paratha.jpg",
+        name: "ALOO PARATHA",
+        category: "breakfast",
+        day: "THURSDAY",
+        description:
+          "It Gives a combination of Just Aloo paratha and mint chutney",
+      },
+      {
+        id: 8,
+        image: "images/poha.webp",
+        name: "Poha and Masala Idly",
+        category: "breakfast",
+        day: "FRIDAY",
+        description:
+          "It Gives a combination of Masala Idly, Poha, Chutney, Bread Jam, Bread Omelete, Cut Fruits, Corn Flakes and Milk ",
+      },
+      {
+        id: 9,
+        image: "images/machurian.jpg",
+        name: "Ghee Rice and Gobi Manchurian",
+        category: "dinner",
+        day: "FRIDAY",
+        description:
+          "It Gives a combination of Ghee Rice, Gobi Manchurian and Dal ",
+        
+      },
+      {
+        id: 10,
+        image: "images/pori-chole.jpg",
+        name: "Pori Chole",
+        category: "breakfast",
+        day: "SATURDAY",
+        description:
+          "It Gives a combination of Pori Chole, Bread Jam, Bread Omelete, Cut Fruits, Corn Flakes and Milk ",
+      },
+      {
+        id: 11,
+        image: "images/cornflakes.jpg",
+        name: "Cornflakes and milk",
+        category: "breakfast",
+        day: "SUNDAY",
+        description:
+          "It Gives a combination of Bread Jam, Corn Flakes and Milk ",
+      },
+     
+      {
+        id: 12,
+        image: "images/biryani.webp",
+        name: "Paneer Biryani, Raita and Sweet",
+        category: "lunch",
+        day: "SUNDAY",
+        description:
+          "It Gives a combination of Paneer Biryani, Raita and Sweet ",
+      },
+    
+    
+     
+      {
+        id: 13,
+        image: "images/no-food.gif",
+        name: "Explore OUT",
+        category: "dinner",
+        day: "Sunday",
+        description:
+          "Mess is closed",
+        
+      },
+
+
+  ];
+  
+  export default Menu;
